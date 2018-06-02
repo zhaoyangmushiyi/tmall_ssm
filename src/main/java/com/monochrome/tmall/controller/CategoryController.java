ebb0108ddc681ed16e4129aee88560cadf135eb2
Monochrome <zhaoyangmushiyi@gmail.com>
2018-06-02 13:25:40 +0800

    Add CategoryController.add

--- src/main/java/com/monochrome/tmall/controller/CategoryController.java
+++ src/main/java/com/monochrome/tmall/controller/CategoryController.java
@@ -2,12 +2,19 @@ package com.monochrome.tmall.controller;
 
 import com.monochrome.tmall.service.CategoryService;
 import com.monochrome.tmall.pojo.Category;
+import com.monochrome.tmall.util.ImageUtil;
 import com.monochrome.tmall.util.Page;
+import com.monochrome.tmall.util.UploadImageFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -22,5 +29,19 @@ public class CategoryController {
         model.addAttribute("cs", cs);
         model.addAttribute("page", page);
         return "admin/listCategory";
+    }
+
+    @RequestMapping("admin_category_add")
+    public String add(Category category, HttpSession session, UploadImageFile uploadImageFile) throws IOException {
+        categoryService.add(category);
+        File imageFolder = new File(session.getServletContext().getRealPath("img/category"));
+        File file = new File(imageFolder, category.getId() + ".jpg");
+        if (!file.getParentFile().exists()) {
+            file.getParentFile().mkdirs();
+        }
+        uploadImageFile.getImage().transferTo(file);
+        BufferedImage bufferedImage = ImageUtil.change2jsp(file);
+        ImageIO.write(bufferedImage, "jpg", file);
+        return "redirect:/admin_category_list";
     }
 }
